1482e21a3d156a1e1f20c95b0872dd66105c817e
alex100588 <alexandru_ionasc@yahoo.com>
2025-03-06 17:35:00 +0200

    adding a spinner

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,25 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     // controllers allowed as to abort or cancell assincronous operations
     const controller = new AbortController()
 
+    setLoading(true)
+
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {signal: controller.signal})
-      .then((res) => setUsers(res.data))
+      .then((res) =>{
+        setUsers(res.data)
+        setLoading(false)
+      })
       .catch((err) => {
         if(err instanceof CanceledError) return
         setError(err.message)
-      });
+        setLoading(false)
+      })
       
       return () => controller.abort()
   }, []);
@@ -28,12 +35,12 @@ function App() {
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {loading && <div className="spinner-border">{loading}</div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
-      ;
     </>
   );
 }
